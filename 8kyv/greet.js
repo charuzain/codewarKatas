cfd313e82de7340aa9c98df0d356338da87f9246
charuzain <charuabhizain@gmail.com>
2022-05-05 21:07:37 -0400

    returning strings

---
+++ 8kyv/greet.js
@@ -0,0 +1,12 @@
+// Returning Strings
+/* Instructions
+Output
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?". */
+
+
+
+const greet = (name)=>{
+  return `Hello, ${name} how are you doing today?`;
+};
+
+console.log(greet("Ryan"));
