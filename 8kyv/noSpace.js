bae83bddc1360b3f401fd18360f82f62b72bef21
charuzain <charuabhizain@gmail.com>
2022-05-08 00:40:30 -0400

    Remove String Spaces refactored

--- 8kyv/noSpace.js
+++ 8kyv/noSpace.js
@@ -3,7 +3,7 @@
 
 const noSpace = (x)=>{
   // return  x.trim().split("").filter(elem => elem !== " ").join("");
-  return x.split(" ").join("")
+  return x.split(" ").join("");
 };
 
 console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
