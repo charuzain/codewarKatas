24e377395f91c8a464d703fdb7c2c4204a5e47c9
charuzain <charuabhizain@gmail.com>
2022-05-07 23:28:01 -0400

    code refactored

--- 8kyv/noSpace.js
+++ 8kyv/noSpace.js
@@ -2,7 +2,8 @@
 // Simple, remove the spaces from the string, then return the resultant string.
 
 const noSpace = (x)=>{
-  return  x.trim().split("").filter(elem => elem !== " ").join("");
+  // return  x.trim().split("").filter(elem => elem !== " ").join("");
+  return x.split(" ").join("")
 };
 
 console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
