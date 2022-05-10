6c94b87c0632bfe3814701cb4cb02a29ae7cd5b1
charuzain <charuabhizain@gmail.com>
2022-05-09 22:38:21 -0400

    Lost without a map

---
+++ 8kyv/map.js
@@ -0,0 +1,7 @@
+// Lost Without a Map
+// Instructions
+// Given an array of integers, return a new array with each value doubled
+
+const maps = (x)=> x.map(e=> e * 2);
+
+console.log(maps([4, 1, 1, 1, 4]));
