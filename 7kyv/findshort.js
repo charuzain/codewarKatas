27e100bf483b7815f92bab2984fc25b1265ef0db
charuzain <charuabhizain@gmail.com>
2022-05-10 23:20:46 -0400

    shortest word refactored

--- 7kyv/findshort.js
+++ 7kyv/findshort.js
@@ -3,13 +3,14 @@
 
 // String will never be empty and you do not need to account for different data types.
 
-const findShort = (s)=>{
-  let array = s.split(" ");
-  let output = [];
-  for (let elem of array) {
-    output.push(elem.length);
-  }
-  return Math.min(...output);
-};
+// const findShort = (s)=>{
+//   let array = s.split(" ");
+//   let output = [];
+//   for (let elem of array) {
+//     output.push(elem.length);
+//   }
+//   return Math.min(...output);
+// };
+const findShort = s=> Math.min(...s.split(" ").map(e=>e.length));
 
 console.log(findShort(("bitcoin take over the world maybe who knows perhaps")));
