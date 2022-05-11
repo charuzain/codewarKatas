2993bafa3e748f1da7e2bd182dc3c90b72e4f66b
charuzain <charuabhizain@gmail.com>
2022-05-11 07:09:08 -0400

    odd number o times refactored

--- 6kyv/findOdd.js
+++ 6kyv/findOdd.js
@@ -9,18 +9,24 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-const findOdd = (A)=>{
-  let output = {};
-  for (let elem of A) {
-    if (!output[elem]) {
-      output[elem] = 1;
-    } else {
-      output[elem]++;
-    }
-  }
-  for (let key in output) {
-    if (output[key] % 2 !== 0) {
-      return Number(key);
-    }
-  }
+// const findOdd = (A)=>{
+//   let output = {};
+//   for (let elem of A) {
+//     if (!output[elem]) {
+//       output[elem] = 1;
+//     } else {
+//       output[elem]++;
+//     }
+//   }
+//   for (let key in output) {
+//     if (output[key] % 2 !== 0) {
+//       return Number(key);
+//     }
+//   }
+// };
+
+const findOdd = (A) => {
+  return A.find(number=> A.filter(n => n === number).length % 2 !== 0);
 };
+
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]));
