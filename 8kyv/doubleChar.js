f2fa59663f9402cc30daf4779dfabdaaf12ef51c
charuzain <charuabhizain@gmail.com>
2022-05-16 23:52:45 -0400

    double char

---
+++ 8kyv/doubleChar.js
@@ -0,0 +1,20 @@
+// Double Char
+// Instructions
+// Output
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+const doubleChar = (str)=>{
+  let output = "";
+  for (let char of str) {
+    output += char.repeat(2);
+  }
+  return output;
+};
+
+console.log(doubleChar("illuminati"));
